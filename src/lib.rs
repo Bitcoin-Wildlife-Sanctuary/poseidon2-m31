952823f2cc23c499a0ebb5015f926df9fd1a65b8
weikengchen <w.k@berkeley.edu>
2024-12-16 22:32:41 +0800

    add Poseidon2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod params;
+
 use crate::params::{FIRST_FOUR_ROUND_RC, LAST_FOUR_ROUNDS_RC, MAT_DIAG16_M_1, PARTIAL_ROUNDS_RC};
 use sha2::Digest;
+use std::cmp::PartialEq;
 
 pub fn modp(a: u32) -> u32 {
     a % ((1 << 31) - 1)
@@ -166,9 +168,199 @@ pub fn poseidon2_permute(state: &mut [u32; 16]) {
     }
 }
 
+pub struct Poseidon2Hasher {
+    pub buffer: Vec<u32>,
+}
+
+impl Poseidon2Hasher {
+    pub fn new() -> Self {
+        Self { buffer: Vec::new() }
+    }
+
+    pub fn update(&mut self, v: impl AsRef<[u32]>) {
+        self.buffer.extend_from_slice(v.as_ref())
+    }
+
+    pub fn finalize(&self, description: impl ToString) -> [u32; 8] {
+        let mut input = self.buffer.clone();
+        let l = input.len();
+
+        let iv = compute_iv_values(
+            format!(
+                "Poseidon2 M31 hashing {} M31 elements for {}",
+                l,
+                description.to_string()
+            )
+            .as_bytes(),
+        );
+
+        let mut state = [
+            0, 0, 0, 0, 0, 0, 0, 0, iv[0], iv[1], iv[2], iv[3], iv[4], iv[5], iv[6], iv[7],
+        ];
+        input.resize(l.div_ceil(8) * 8, 0);
+
+        for chunk in input.chunks(8) {
+            state[0..8].copy_from_slice(chunk);
+            poseidon2_permute(&mut state);
+        }
+
+        [
+            state[0], state[1], state[2], state[3], state[4], state[5], state[6], state[7],
+        ]
+    }
+
+    pub fn finalize_reset(&mut self, description: impl ToString) -> [u32; 8] {
+        let res = self.finalize(description);
+        self.buffer.clear();
+        res
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum Poseidon2Mode {
+    ABSORB,
+    SQUEEZE,
+}
+
+pub struct Poseidon2Sponge {
+    pub state: [u32; 16],
+    pub buffer: Vec<u32>,
+    pub mode: Poseidon2Mode,
+    pub squeeze_index: usize,
+}
+
+impl Poseidon2Sponge {
+    pub fn new(description: impl ToString) -> Self {
+        let iv = compute_iv_values(
+            format!("Poseidon2 M31 sponge for {}", description.to_string()).as_bytes(),
+        );
+        Self {
+            state: [
+                0, 0, 0, 0, 0, 0, 0, 0, iv[0], iv[1], iv[2], iv[3], iv[4], iv[5], iv[6], iv[7],
+            ],
+            buffer: Vec::new(),
+            mode: Poseidon2Mode::ABSORB,
+            squeeze_index: 0,
+        }
+    }
+
+    pub fn absorb(&mut self, data: &[u32]) {
+        assert!(!data.is_empty());
+
+        self.mode = Poseidon2Mode::ABSORB;
+        self.squeeze_index = 0;
+
+        let l = self.buffer.len() + data.len();
+
+        if l >= 8 {
+            self.state[0..self.buffer.len()].copy_from_slice(self.buffer.as_slice());
+            self.state[self.buffer.len()..8].copy_from_slice(&data[0..8 - self.buffer.len()]);
+            self.buffer.clear();
+            poseidon2_permute(&mut self.state);
+
+            self.absorb(&data[8 - self.buffer.len()..]);
+        } else {
+            self.buffer.extend_from_slice(data);
+        }
+    }
+
+    pub fn squeeze(&mut self, size: usize) -> Vec<u32> {
+        assert!(size > 0);
+
+        if self.mode == Poseidon2Mode::ABSORB && !self.buffer.is_empty() {
+            for i in 0..8 {
+                self.state[i] = 0;
+            }
+            self.state[0..self.buffer.len()].copy_from_slice(self.buffer.as_slice());
+            self.buffer.clear();
+            poseidon2_permute(&mut self.state);
+
+            self.squeeze_index = 0;
+        }
+
+        self.mode = Poseidon2Mode::SQUEEZE;
+
+        let mut res = vec![];
+
+        while res.len() < size {
+            let need = size - res.len();
+            let ready = 8 - self.squeeze_index;
+
+            if ready > need {
+                res.extend_from_slice(&self.state[self.squeeze_index..self.squeeze_index + need]);
+                self.squeeze_index += need;
+            } else {
+                res.extend_from_slice(&self.state[self.squeeze_index..8]);
+
+                for i in 0..8 {
+                    self.state[i] = 0;
+                }
+                poseidon2_permute(&mut self.state);
+                self.squeeze_index = 0;
+            }
+        }
+
+        res
+    }
+}
+
+pub struct Poseidon2CRH;
+
+impl Poseidon2CRH {
+    pub fn hash_fixed_length(data: &[u32]) -> [u32; 8] {
+        let mut cur_layer = vec![];
+        for chunk in data.chunks(16) {
+            let mut cur = [0u32; 16];
+            cur[0..chunk.len()].copy_from_slice(chunk);
+
+            poseidon2_permute(&mut cur);
+
+            for i in 0..std::cmp::min(8, chunk.len()) {
+                cur[i] = addmod(cur[i], chunk[i]);
+            }
+
+            cur_layer.push([
+                cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7],
+            ]);
+        }
+
+        while cur_layer.len() > 1 {
+            let mut new_layer = vec![];
+            for chunk in cur_layer.chunks(2) {
+                if chunk.len() == 1 {
+                    new_layer.push(chunk[0]);
+                } else {
+                    let mut cur = [0u32; 16];
+                    for i in 0..8 {
+                        cur[i] = chunk[0][i];
+                        cur[i + 8] = chunk[1][i];
+                    }
+
+                    poseidon2_permute(&mut cur);
+
+                    for i in 0..8 {
+                        cur[i] = addmod(cur[i], chunk[0][i]);
+                    }
+
+                    new_layer.push([
+                        cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7],
+                    ]);
+                }
+            }
+
+            cur_layer = new_layer;
+        }
+
+        cur_layer[0]
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::poseidon2_permute;
+    use crate::{modp, poseidon2_permute, Poseidon2CRH, Poseidon2Sponge};
+    use rand::Rng;
+    use rand_chacha::rand_core::SeedableRng;
+    use rand_chacha::ChaCha20Rng;
 
     #[test]
     fn test_poseidon2_permute() {
@@ -181,5 +373,101 @@ mod tests {
             58674647, 323699327,
         ];
         assert_eq!(state, expected);
+    }
+
+    #[test]
+    fn test_absorb_and_squeeze() {
+        let mut sponge = Poseidon2Sponge::new("test");
+        let c1 = sponge.state[8..16].to_vec();
+
+        sponge.absorb(&[1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let c2 = {
+            let mut state = [
+                1, 2, 3, 4, 5, 6, 7, 8, c1[0], c1[1], c1[2], c1[3], c1[4], c1[5], c1[6], c1[7],
+            ];
+            poseidon2_permute(&mut state);
+            state[8..16].to_vec()
+        };
+
+        assert_eq!(sponge.state[8..16], c2);
+
+        let a = sponge.squeeze(1);
+
+        let c3_full = {
+            let mut state = [
+                9, 0, 0, 0, 0, 0, 0, 0, c2[0], c2[1], c2[2], c2[3], c2[4], c2[5], c2[6], c2[7],
+            ];
+            poseidon2_permute(&mut state);
+            state
+        };
+
+        assert_eq!(a, vec![c3_full[0]]);
+
+        let a = sponge.squeeze(2);
+        assert_eq!(a, vec![c3_full[1], c3_full[2]]);
+
+        sponge.absorb(&[10, 11, 12]);
+        sponge.absorb(&[13]);
+
+        let a = sponge.squeeze(3);
+
+        let c3 = c3_full[8..16].to_vec();
+
+        let c4_full = {
+            let mut state = [
+                10, 11, 12, 13, 0, 0, 0, 0, c3[0], c3[1], c3[2], c3[3], c3[4], c3[5], c3[6], c3[7],
+            ];
+            poseidon2_permute(&mut state);
+            state
+        };
+        assert_eq!(a, vec![c4_full[0], c4_full[1], c4_full[2]]);
+
+        let _ = sponge.squeeze(8);
+        let a = sponge.squeeze(1);
+
+        let c4 = c4_full[8..16].to_vec();
+
+        let c5_full = {
+            let mut state = [
+                0, 0, 0, 0, 0, 0, 0, 0, c4[0], c4[1], c4[2], c4[3], c4[4], c4[5], c4[6], c4[7],
+            ];
+            poseidon2_permute(&mut state);
+            state
+        };
+        assert_eq!(a, vec![c5_full[3]]);
+
+        sponge.absorb(&[13]);
+        let a = sponge.squeeze(2);
+
+        let c5 = c5_full[8..16].to_vec();
+        let c6_full = {
+            let mut state = [
+                13, 0, 0, 0, 0, 0, 0, 0, c5[0], c5[1], c5[2], c5[3], c5[4], c5[5], c5[6], c5[7],
+            ];
+            poseidon2_permute(&mut state);
+            state
+        };
+
+        assert_eq!(a, vec![c6_full[0], c6_full[1]]);
+    }
+
+    #[test]
+    fn test_commit() {
+        let mut prng = ChaCha20Rng::seed_from_u64(0);
+
+        let mut a = [0u32; 5];
+        let mut b = [0u32; 17];
+
+        for i in 0..5 {
+            a[i] = modp(prng.gen::<u32>());
+        }
+
+        for i in 0..17 {
+            b[i] = modp(prng.gen::<u32>());
+        }
+
+        let _ = Poseidon2CRH::hash_fixed_length(&a);
+        let _ = Poseidon2CRH::hash_fixed_length(&b);
     }
 }
